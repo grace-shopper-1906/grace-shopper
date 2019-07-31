c1949c56059e6c2eadd7709865c213595836f051
Sarah Ahmad <CoffeeJavaBeans@sarahmahmad.com>
2019-07-31 11:53:43 -0500

    redux connect gets product on state

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,8 +1,19 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
+import {getOneProduct} from '../store/oneProduct.js'
 
 export class OneProduct extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  componentDidMount() {
+    this.props.getOneProduct(this.props.match.params.id)
+    this.setState(this.props.match.params)
+  }
+
   render() {
     const main = (
       <div>
@@ -13,4 +24,16 @@ export class OneProduct extends Component {
   }
 }
 
-export default withRouter(connect(null)(OneProduct))
+const mapStateToProps = state => ({
+  oneProduct: state.oneProduct
+})
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getOneProduct: id => dispatch(getOneProduct(id))
+  }
+}
+
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(OneProduct)
+)
