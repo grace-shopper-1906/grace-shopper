f7801e728d12ea3bd726e7fc48c40dbee7369c22
Sarah Ahmad <CoffeeJavaBeans@sarahmahmad.com>
2019-08-05 15:49:18 -0500

    front end only

--- client/components/ReviewForm.js
+++ client/components/ReviewForm.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter} from 'react-router-dom'
-import {Container, Form, Input, Button} from 'semantic-ui-react'
+import {
+  Container,
+  Form,
+  TextArea,
+  Rating,
+  Input,
+  Button
+} from 'semantic-ui-react'
 
 export class ReviewForm extends React.Component {
   constructor(props) {
@@ -38,18 +45,19 @@ export class ReviewForm extends React.Component {
       <Container>
         <h1>Review:</h1>
         <Form onSubmit={this.handleSubmit}>
-          <Form.Group>
-            <Form.Field
-              id="review"
-              name="review"
-              type="text"
-              onChange={this.handleChange}
-              value={this.state.firstName}
-              control={Input}
-              //label="Review"
-              placeholder="Write Review Here"
-            />
-          </Form.Group>
+          <Rating icon="star" defaultRating={1} maxRating={5}>
+            Star given: {2}
+          </Rating>
+          <TextArea
+            className="reviewSize"
+            id="review"
+            name="review"
+            type="text"
+            onChange={this.handleChange}
+            value={this.state.firstName}
+            //label="Review"
+            placeholder="Write Review Here"
+          />
 
           <Form.Field
             id="form-button-control-public"
